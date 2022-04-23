cb7fde0059bc25c89ae788100b09c928e3729c78
Wesley <fernandess.weslley@gmail.com>
2022-04-22 22:50:04 -0300

    uptade account

--- src/index.js
+++ src/index.js
@@ -102,4 +102,30 @@ app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).json({ sucess: "Sucess"});
 });
 
+app.get('/statement/date', verifyIfExistsAccountCPF,(request, response) => {
+
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+  return response.json(statement)
+});
+
+app.put('/account', verifyIfExistsAccountCPF,(request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(200).json({ sucess: "Name updated"});
+})
+
+app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer)
+})
 app.listen(3333);
